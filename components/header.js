1bf8275968879e44d6058a2f0dcf1cfe1663d4e0
Sean Callan <seancallan@gmail.com>
2021-02-03 21:25:27 -0700

    Fix warning

--- components/header.js
+++ components/header.js
@@ -5,11 +5,11 @@ export default function Header({ subtitle }) {
   <section className="flex-col flex items-left mt-20">
     <div className="flex-row flex items-left text-left">
       <Link href="/">
-      <a class="flex title-font font-medium items-center md:justify-start justify-center mr-4">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 hover:bg-red-600 rounded-full" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-        </svg>
-      </a>
+        <a className="flex title-font font-medium items-center md:justify-start justify-center mr-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 hover:bg-red-600 rounded-full" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+          </svg>
+        </a>
       </Link>
       <a href="/articles" className="text-3xl text-red-500 hover:text-red-600 font-black hover:underline mr-4">Articles</a>
       <a href="https://github.com/utensils" target="_blank" className="text-3xl text-red-500 hover:text-red-600 font-black hover:underline">GitHub</a>
