7d11c560f708ca88b8ff2826edb83103b21bf4c9
James Brink <brink.james@gmail.com>
2025-04-02 06:49:52 -0700

    fix(ui): improve tag component wrapping on mobile layouts

--- components/tags.js
+++ components/tags.js
@@ -2,9 +2,9 @@ export default function Tags({ tags = [] }) {
   if (!tags || tags.length === 0) return null
 
   return (
-    <div className="inline-flex">
+    <div className="flex flex-wrap items-center">
       {tags.map((tag) => (
-        <span key={tag} className={`py-1 px-2 mr-2 rounded bg-red-50 text-red-500 text-xs font-black tracking-widest`}>{tag}</span>
+        <span key={tag} className={`py-1 px-2 mr-2 mb-2 rounded bg-red-50 text-red-500 text-xs font-black tracking-widest`}>{tag}</span>
       ))}
     </div>
   )
